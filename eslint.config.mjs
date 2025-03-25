e56a18c1fe04bbfcdcddaa4cfdf11197924c8ce6
Tristan White <113216091+up2code3@users.noreply.github.com>
2025-03-25 09:58:34 -0400

    fixed packj and lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,23 @@
-import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    // Include the recommended configuration directly, no need for 'extends'
+    rules: js.configs.recommended.rules,
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { react: pluginReact },
+    rules: pluginReact.configs.flat.recommended.rules,
+  },
+];
