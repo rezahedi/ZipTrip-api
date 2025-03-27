569509b9be289bad6eb51af56f1abb0248637589
Reza Zahedi <r.zahedi@gmail.com>
2025-03-27 16:21:07 -0700

    Run format

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const { PORT = 8000 } = process.env;
-const app = require("./app");
+const { PORT = 8000 } = process.env
+const app = require('./app')
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+const listener = () => console.log(`Listening on Port ${PORT}!`)
+app.listen(PORT, listener)
