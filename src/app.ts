2c01e018475b3cec81ba85add3bd504c14b995c8
Reza Zahedi <r.zahedi@gmail.com>
2025-04-06 10:52:12 -0700

    Create api swagger doc

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import favicon from 'express-favicon'
 import logger from 'morgan'
 import router from './routes/mainRouter'
 import plansRouter from './routes/plans'
+import swaggerUI from 'swagger-ui-express'
+import YAML from 'yamljs'
 
 const app = express()
 
@@ -18,5 +20,8 @@ app.use(favicon(__dirname + '/public/favicon.ico'))
 // routes
 app.use('/api/v1', router)
 app.use('/api/v1/plans', plansRouter)
+
+const swaggerDocument = YAML.load('./swagger.yaml')
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 export default app
