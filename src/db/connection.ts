2a8efbe2831a97e8ebc1265a6849478f9b0357e4
Sisi Wang <sisiwang242@gmail.com>
2025-04-08 19:37:51 -0400

    Set up MongoDB connection

---
+++ src/db/connection.ts
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = (url: string) => {
+    if (!url) {
+        throw new Error("Database connection string is undefined. Check your environment variables.");
+    }
+    return mongoose.connect(url, {
+        serverSelectionTimeoutMS: 5000,
+    })
+    .then(() => {
+        console.log("Connected to the DB ...");
+    })
+    .catch((error: string) => {
+        console.error("Database connection Error: ", error);
+        process.exit(1);
+    })
+}
+
+export { connectDB };
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
+require('dotenv').config();
 const { PORT = 8000 } = process.env
 import app from './app'
+import { connectDB } from './db/connection';
 
-const listener = () => console.log(`Listening on Port ${PORT}!`)
-app.listen(PORT, listener)
+
+const listener = async () => {
+    try {
+        await connectDB(`${process.env.MONGO_URI}`);
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}...`);
+        });
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+listener();
