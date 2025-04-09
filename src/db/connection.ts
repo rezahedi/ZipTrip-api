503f5be433e784e8eec65e542a1557a7c5974212
Sisi Wang <sisiwang242@gmail.com>
2025-04-08 21:57:48 -0400

    fix error

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const connectDB = (url: string) => {
-    if (!url) {
-        throw new Error("Database connection string is undefined. Check your environment variables.");
-    }
-    return mongoose.connect(url, {
-        serverSelectionTimeoutMS: 5000,
+  if (!url) {
+    throw new Error('Database connection string is undefined. Check your environment variables.')
+  }
+  return mongoose
+    .connect(url, {
+      serverSelectionTimeoutMS: 5000,
     })
     .then(() => {
-        console.log("Connected to the DB ...");
+      console.log('Connected to the DB ...')
     })
     .catch((error: string) => {
-        console.error("Database connection Error: ", error);
-        process.exit(1);
+      console.error('Database connection Error: ', error)
+      process.exit(1)
     })
 }
 
-export { connectDB };
+export { connectDB }
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-require('dotenv').config();
+require('dotenv').config()
 const { PORT = 8000 } = process.env
 import app from './app'
-import { connectDB } from './db/connection';
-
+import { connectDB } from './db/connection'
 
 const listener = async () => {
-    try {
-        await connectDB(`${process.env.MONGO_URI}`);
-        app.listen(PORT, () => {
-            console.log(`Server is listening on port ${PORT}...`);
-        });
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-};
+  try {
+    await connectDB(`${process.env.MONGO_URI}`)
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}...`)
+    })
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
 
-listener();
+listener()
