f1eea466762b5587fceb4d38c7d9cee502b24979
Sisi Wang <sisiwang242@gmail.com>
2025-04-08 22:19:22 -0400

    fix error

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,7 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 const connectDB = (url: string) => {
   if (!url) {
