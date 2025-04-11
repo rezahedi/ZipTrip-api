ddf5c4ec2a15802f45483b7e7a06905386f4441d
Reza Zahedi <r.zahedi@gmail.com>
2025-04-10 17:14:03 -0700

    Remove dist folder

--- dist/controllers/mainController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mainController = (req, res) => {
-    res.json({
-        data: 'This is a full stack app!',
-    });
-};
-exports.default = mainController;
