b6489b947a35b3a172dba98f2e01f45558852954
Sisi Wang <sisiwang242@gmail.com>
2025-04-15 19:39:06 -0400

    run prettier to fix error

--- src/errors/custom_error.ts
+++ src/errors/custom_error.ts
@@ -1,13 +1,12 @@
 class CustomAPIError extends Error {
-    statusCode: number
-  
-    constructor(message: string, statusCode: number = 400) {
-      super(message)
-      this.statusCode = statusCode
-  
-      Object.setPrototypeOf(this, CustomAPIError.prototype)
-    }
+  statusCode: number
+
+  constructor(message: string, statusCode: number = 400) {
+    super(message)
+    this.statusCode = statusCode
+
+    Object.setPrototypeOf(this, CustomAPIError.prototype)
   }
-  
-  export default CustomAPIError
-  
+}
+
+export default CustomAPIError
