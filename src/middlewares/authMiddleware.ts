cbd3ddc1e2e56a0bdeb4a2c5878f0ab40059a8af
Reza Zahedi <r.zahedi@gmail.com>
2025-04-14 21:59:33 -0700

    Add fake auth data to auth middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,6 +3,15 @@ import { Request, Response, NextFunction } from 'express'
 const authMiddleware = async (request: Request, response: Response, next: NextFunction) => {
   // TODO: Authentication logic here
 
+  // TODO: This below code should replace with real authentication
+  // FIXME: Populate request.user with fake user for now
+  request.user = {
+    userId: '67c636912f64a2dedb34726d',
+    name: 'Reza Fake',
+    email: 'reza@fake.com',
+    token: 'some-fake-token-sdfjsdkfjoweifjsodlf',
+  }
+
   next()
 }
 
