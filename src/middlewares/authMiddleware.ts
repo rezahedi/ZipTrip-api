98a5b94c6257e7029389ab89e2b9d20b5b9c66b6
Reza Zahedi <r.zahedi@gmail.com>
2025-04-15 22:32:29 -0700

    Make auth middleware works with fake data temporary

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,21 @@
 import { Request, Response, NextFunction } from 'express'
 
 const authMiddleware = async (request: Request, response: Response, next: NextFunction) => {
+  const authHeader = request.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    // FIXME: Replace below response with custom error handler
+    response.status(401).json({ error: 'Authentication not provided' })
+    return
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  if (token !== 'fake-temporary-token') {
+    // FIXME: Replace below response with custom error handler
+    response.status(401).json({ error: 'Invalid token' })
+    return
+  }
+
   // TODO: Get payload out of token
   // TODO: Fetch user's detail from database
   // TODO: Return user's detail as request.user
@@ -11,7 +26,7 @@ const authMiddleware = async (request: Request, response: Response, next: NextFu
     userId: '67fdfdfcd9c143f362fb9701',
     name: 'Reza',
     email: 'reza@example.com',
-    token: 'some-fake-token-sdfjsdkfjoweifjsodlf',
+    token: 'fake-temporary-token',
   }
 
   next()
