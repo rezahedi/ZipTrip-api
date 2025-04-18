a8766e83686300d73ebbe7b6c59abca36a3a6425
Reza Zahedi <r.zahedi@gmail.com>
2025-04-18 00:04:11 -0700

    feat: fetch cat's detail with its plans

--- src/controllers/plans.ts
+++ src/controllers/plans.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import PlanSchema from '../models/Plans'
 import UserSchema, { IUser } from '../models/Users'
+import CategorySchema, { ICategory } from '../models/Categories'
 import NotFoundError from '../errors/not_found'
 
 import dummyData from '../dummyData.json'
@@ -49,7 +50,6 @@ const fetchUserWithPlans = async (req: Request, res: Response) => {
   const pageNumber: number = (parseInt(page as string) - 1) * pageSize
 
   const user: IUser | null = await UserSchema.findById(userId).select('-password').lean()
-  console.log(user)
 
   if (!user) {
     throw new NotFoundError(`User with id ${userId} not found`)
@@ -75,36 +75,31 @@ const fetchUserWithPlans = async (req: Request, res: Response) => {
   })
 }
 
-const fetchCategoryWithPlans = (req: Request, res: Response) => {
-  const { categoryId, page = 1, size = PAGE_SIZE } = req.params
+const fetchCategoryWithPlans = async (req: Request, res: Response) => {
+  const { categoryId } = req.params
+  const { page = 1, size = PAGE_SIZE } = req.query
 
-  const category = dummyCategories.find((category: categoryType) => {
-    return category.categoryId === categoryId
-  })
+  const pageSize: number = parseInt(size as string)
+  const pageNumber: number = (parseInt(page as string) - 1) * pageSize
 
-  if (!category) {
-    res.status(404).json({
-      error: `Category with id ${categoryId} not found`,
-    })
-    return
+  const category: ICategory | null = await CategorySchema.findById(categoryId).lean()
+
+  const filters = {
+    categoryId,
   }
 
-  let resultPlans: planType[] = dummyPlans
-  resultPlans = resultPlans.filter((plan: planType) => {
-    return plan.categoryId === categoryId
-  })
+  const plans = await PlanSchema.find(filters)
+    .populate('categoryId', 'name')
+    .populate('userId', 'name')
+    .skip(pageNumber)
+    .limit(pageSize)
 
-  // Pagination
-  const pageNumber = parseInt(page as string)
-  const pageSize = parseInt(size as string)
-  resultPlans = resultPlans.slice((pageNumber - 1) * pageSize, pageNumber * pageSize)
-
-  res.json({
+  res.status(StatusCodes.OK).json({
     ...category,
     plans: {
       page: pageNumber,
       size: pageSize,
-      items: resultPlans,
+      items: plans,
     },
   })
 }
