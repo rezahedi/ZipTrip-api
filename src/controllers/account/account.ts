82b330a5915c26761ccec6d94351b6fa8761b538
Reza Zahedi <r.zahedi@gmail.com>
2025-04-20 21:33:27 -0700

    feat: create fetch all & create new category routes

--- src/controllers/account/account.ts
+++ src/controllers/account/account.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
-import PlanSchema, { IPlan } from '../models/Plans'
-import StopSchema, { IStop } from '../models/Stops'
+import PlanSchema, { IPlan } from '../../models/Plans'
+import StopSchema, { IStop } from '../../models/Stops'
 import mongoose from 'mongoose'
 
 const fetchAllPlans = async (req: Request, res: Response) => {
---
+++ src/controllers/account/index.ts
@@ -0,0 +1,4 @@
+import { fetchAllPlans, createNewPlan, fetchPlan, updatePlan, deletePlan } from './account'
+import { fetchAllCategories, createNewCategory } from './categories'
+
+export { fetchAllPlans, createNewPlan, fetchPlan, updatePlan, deletePlan, fetchAllCategories, createNewCategory }
