c193692ba3446cde3e7062a23df9cbc30984d82e
Reza Zahedi <r.zahedi@gmail.com>
2025-04-20 22:28:02 -0700

    remove unused stops routes and controllers

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -8,15 +8,11 @@ import {
   fetchAllCategories,
   createNewCategory,
 } from '../controllers/account'
-import { createNewStop, fetchStop, updateStop, deleteStop } from '../controllers/stops'
 
 const router = express.Router()
 
 router.route('/plans/').get(fetchAllPlans).post(createNewPlan)
 router.route('/plans/:planId').get(fetchPlan).put(updatePlan).delete(deletePlan)
-// TODO: Add POST /:planId/stops endpoint to the swagger.yaml file.
-router.route('/plans/:planId/stops').post(createNewStop)
-router.route('/plans/:planId/stops/:stopId').get(fetchStop).put(updateStop).delete(deleteStop)
 router.route('/categories').get(fetchAllCategories).post(createNewCategory)
 
 export default router
