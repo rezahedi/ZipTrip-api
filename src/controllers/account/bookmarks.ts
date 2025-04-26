7052aa0d1bec18d6ab563763f696006690c8d370
Reza Zahedi <r.zahedi@gmail.com>
2025-04-25 19:54:49 -0700

    refactor: change response body format

--- src/controllers/account/bookmarks.ts
+++ src/controllers/account/bookmarks.ts
@@ -6,9 +6,23 @@ import PlanSchema from '../../models/Plans'
 const fetchAllBookmarkedPlans = async (req: Request, res: Response) => {
   if (!req.user) throw new Error('Not authorized to access.')
 
-  const bookmarks = await BookmarkSchema.find({ userId: req.user.userId })
+  const filters = {
+    _id: {
+      $in: await BookmarkSchema.find({
+        userId: req.user.userId,
+      }).distinct('planId'),
+    },
+  }
 
-  res.status(StatusCodes.OK).json(bookmarks)
+  // TODO: Add pagination support later
+
+  const bookmarkedPlans = await PlanSchema.find(filters).populate('categoryId', 'name').populate('userId', 'name')
+
+  res.status(StatusCodes.OK).json({
+    page: 1,
+    size: 10,
+    items: bookmarkedPlans,
+  })
 }
 
 const bookmarkPlan = async (req: Request, res: Response) => {
@@ -20,12 +34,12 @@ const bookmarkPlan = async (req: Request, res: Response) => {
   await PlanSchema.findById(planId)
 
   // Create a new bookmark
-  const newBookmark: IBookmark = await BookmarkSchema.create({
+  await BookmarkSchema.create({
     userId: req.user.userId,
     planId,
   })
 
-  res.status(StatusCodes.CREATED).json(newBookmark)
+  res.status(StatusCodes.CREATED).end()
 }
 
 export { fetchAllBookmarkedPlans, bookmarkPlan }
