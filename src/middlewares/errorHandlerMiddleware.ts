d73f434763467e4b1d8f90f26b5f2949e8a9a829
Reza Zahedi <r.zahedi@gmail.com>
2025-10-16 14:51:22 -0700

    remove debugging logs

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -5,7 +5,6 @@ import { MongoServerError } from 'mongodb'
 import CustomAPIError from '../errors/custom_error'
 
 const errorHandlerMiddleware = (err: unknown, req: Request, res: Response, next: NextFunction) => {
-  console.log('Error Handler Middleware', err instanceof MongoServerError, err)
   // Set default or set by custom error's values
   let customError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
