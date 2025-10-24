b123452f7b14b3dddf31a7f86773264014164e0b
Reza Zahedi <r.zahedi@gmail.com>
2025-10-24 16:57:11 -0700

    combine fetch google place into fetch place

--- src/controllers/places.ts
+++ src/controllers/places.ts
@@ -26,12 +26,69 @@ const fetchPlace = async (req: Request, res: Response) => {
     )
     .lean()
 
-  if (!place) throw new CustomAPIError(`No place with id ${placeId}`, StatusCodes.NOT_FOUND)
+  if (place) {
+    res.status(StatusCodes.OK).json({
+      ...place,
+      location: geoJsonToCoords(place.location),
+    })
+  } else {
+    // Check if the place is already in our db
+    const existingPlace: IGooglePlace | null = await GooglePlaceSchema.findOne({
+      googlePlaceId: placeId,
+      version: GOOGLE_PLACE_FETCH_VERSION,
+    }).lean()
+    if (existingPlace) {
+      const data = JSON.parse(existingPlace.data)
+      res.status(StatusCodes.OK).json(transformGooglePlaceToSchema(data))
+    } else {
+      const apiKey = process.env.GOOGLE_MAPS_API_KEY as string
+      if (!apiKey)
+        throw new CustomAPIError('Google Places API key is not configured', StatusCodes.INTERNAL_SERVER_ERROR)
+      const fields =
+        'id,displayName,shortFormattedAddress,formattedAddress,addressComponents,location,icon_mask_base_uri,photos,primaryType,iconBackgroundColor,editorialSummary,generativeSummary,reviewSummary,rating,userRatingCount'
+      const url = `https://places.googleapis.com/v1/places/${placeId}?fields=${fields}&key=${apiKey}`
 
-  res.status(StatusCodes.OK).json({
-    ...place,
-    location: geoJsonToCoords(place.location),
-  })
+      const response = await fetch(url)
+      if (response.ok) {
+        const data = await response.json()
+
+        if (data.photos.length > 0) {
+          // if exist get maximum 4 images and store it in Cloudinary
+          for (let i = 0; i < data.photos.length && i < PLACE_IMG_UPLOAD_MAX_COUNT; i++) {
+            if ('name' in data.photos[i]) {
+              const imageURL = `https://places.googleapis.com/v1/${data.photos[i].name}/media?maxHeightPx=400&maxWidthPx=400&key=${process.env.GOOGLE_MAPS_API_KEY}`
+              const { secure_url } = await cloudinary.uploader.upload(imageURL, {
+                folder: 'ZipTrip/Places',
+                overwrite: true,
+                invalidate: true,
+              })
+              data.photos[i].imageURL = secure_url
+            }
+          }
+        }
+
+        // Insert if placeId not found
+        // Update if version was different
+        await GooglePlaceSchema.updateOne(
+          {
+            googlePlaceId: placeId,
+            version: { $ne: GOOGLE_PLACE_FETCH_VERSION },
+          },
+          {
+            $set: {
+              version: GOOGLE_PLACE_FETCH_VERSION,
+              data: JSON.stringify(data),
+            },
+          },
+          { upsert: true }
+        )
+        res.status(StatusCodes.OK).json(transformGooglePlaceToSchema(data))
+      } else {
+        throw new CustomAPIError('Failed to fetch place from Google Places API', StatusCodes.INTERNAL_SERVER_ERROR)
+      }
+    }
+    // res.status(StatusCodes.OK).json({ googlePlaceId: placeId })
+  }
 }
 
 const fetchAllNearbyPlaces = async (req: Request, res: Response) => {
@@ -73,63 +130,4 @@ const fetchAllNearbyPlaces = async (req: Request, res: Response) => {
   })
 }
 
-const fetchGooglePlace = async (req: Request, res: Response) => {
-  // Check if the place is already in our db
-  const { googlePlaceId } = req.params
-  const existingPlace: IGooglePlace | null = await GooglePlaceSchema.findOne({
-    googlePlaceId,
-    version: GOOGLE_PLACE_FETCH_VERSION,
-  }).lean()
-  if (existingPlace) {
-    const data = JSON.parse(existingPlace.data)
-    res.status(StatusCodes.OK).json(transformGooglePlaceToSchema(data))
-  } else {
-    const apiKey = process.env.GOOGLE_MAPS_API_KEY as string
-    if (!apiKey) throw new CustomAPIError('Google Places API key is not configured', StatusCodes.INTERNAL_SERVER_ERROR)
-    const fields =
-      'id,displayName,shortFormattedAddress,formattedAddress,addressComponents,location,icon_mask_base_uri,photos,primaryType,iconBackgroundColor,editorialSummary,generativeSummary,reviewSummary,rating,userRatingCount'
-    const url = `https://places.googleapis.com/v1/places/${googlePlaceId}?fields=${fields}&key=${apiKey}`
-
-    const response = await fetch(url)
-    if (response.ok) {
-      const data = await response.json()
-
-      if (data.photos.length > 0) {
-        // if exist get maximum 4 images and store it in Cloudinary
-        for (let i = 0; i < data.photos.length && i < PLACE_IMG_UPLOAD_MAX_COUNT; i++) {
-          if ('name' in data.photos[i]) {
-            const imageURL = `https://places.googleapis.com/v1/${data.photos[i].name}/media?maxHeightPx=400&maxWidthPx=400&key=${process.env.GOOGLE_MAPS_API_KEY}`
-            const { secure_url } = await cloudinary.uploader.upload(imageURL, {
-              folder: 'ZipTrip/Places',
-              overwrite: true,
-              invalidate: true,
-            })
-            data.photos[i].imageURL = secure_url
-          }
-        }
-      }
-
-      // Insert if placeId not found
-      // Update if version was different
-      await GooglePlaceSchema.updateOne(
-        {
-          googlePlaceId,
-          version: { $ne: GOOGLE_PLACE_FETCH_VERSION },
-        },
-        {
-          $set: {
-            version: GOOGLE_PLACE_FETCH_VERSION,
-            data: JSON.stringify(data),
-          },
-        },
-        { upsert: true }
-      )
-      res.status(StatusCodes.OK).json(transformGooglePlaceToSchema(data))
-    } else {
-      throw new CustomAPIError('Failed to fetch place from Google Places API', StatusCodes.INTERNAL_SERVER_ERROR)
-    }
-  }
-  res.status(StatusCodes.OK).json({ googlePlaceId })
-}
-
-export { fetchAllPlaces, fetchPlace, fetchAllNearbyPlaces, fetchGooglePlace }
+export { fetchAllPlaces, fetchPlace, fetchAllNearbyPlaces }
